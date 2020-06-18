62f54c31f615ab0a9633fae8c1ede6e7c0e34684
Mike Polley <mgp27@kent.ac.uk>
2020-06-18 12:10:21 +0100

    Adding rest of example scripts

--- cta_refinement/__init__.py
+++ cta_refinement/__init__.py
@@ -27,6 +27,30 @@ def fisher():
     src = f.readlines()
     return render_template("fisher.html", src=src, len=len(src))
 
+@app.route("/sample-scripts/ford-credit-portal")
+def ford():
+    f = open("/var/www/cta_refinement/cta_refinement/dbmModules/Examples/FordCreditWebPortal","r")
+    src = f.readlines()
+    return render_template("ford.html", src=src, len=len(src))
+
+@app.route("/sample-scripts/ooi-word-counting")
+def ooi():
+    f = open("/var/www/cta_refinement/cta_refinement/dbmModules/Examples/OOIWordCounting","r")
+    src = f.readlines()
+    return render_template("ooi.html", src=src, len=len(src))
+
+@app.route("/sample-scripts/scheduled-task-protocol")
+def task():
+    f = open("/var/www/cta_refinement/cta_refinement/dbmModules/Examples/ScheduledTaskProtocol","r")
+    src = f.readlines()
+    return render_template("task.html", src=src, len=len(src))
+
+@app.route("/sample-scripts/smtp-client")
+def smtp():
+    f = open("/var/www/cta_refinement/cta_refinement/dbmModules/Examples/SMTPClient","r")
+    src = f.readlines()
+    return render_template("smtp.html", src=src, len=len(src))
+
 
 if __name__ == "__main__":
     app.run(threaded=True)
